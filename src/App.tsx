336b122ad2d6e89d9ff8aeadf803f2d787b760d3
ledemkam <81742990+ledemkam@users.noreply.github.com>
2024-07-28 17:00:42 +0200

    "final implement"

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,59 @@
+import { InferType, reach } from "yup"
+import { userSchema } from "./validations"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+
+type User = InferType<typeof userSchema>
+
 export default function App() {
+
+  const addressSchema = reach(userSchema, "address")
+
+  console.log(addressSchema);
+  
+
+
+
+
+ const { register, handleSubmit, formState: {errors} } = useForm<User>({
+    resolver: yupResolver(userSchema)
+ })
+ 
+const onSubmit = (data:User) =>{
+  console.log(data)
+}
+
   return (
     <>
     <div className="flex items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-          <form className="mt-8">
+          <form className="mt-8" onSubmit={handleSubmit(onSubmit)}>
             <div className="rounded-md shadow-md p-3">
               <div className="mb-4">
                 <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
-                <input type="text" name="name" id="name" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-md focus:outline-none" />
+                <input {...register("name")} type="text" name="name" id="name" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-md focus:outline-none" />
+                {errors.name && <p className="text-red-500 text-xs italic">{errors.name.message}</p>}
               </div>
             </div>
             <div className="rounded-md shadow-md p-3">
               <div className="mb-4">
                 <label htmlFor="age" className="block text-sm font-medium text-gray-700">Age</label>
-                <input type="number" name="age" id="age" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-md focus:outline-none" />
+                <input {...register("age")} type="number" name="age" id="age" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-md focus:outline-none" />
+                {errors.age && <p className="text-red-500 text-xs italic">{errors.age.message}</p>}
               </div>
             </div>
             <div className="rounded-md shadow-md p-3">
               <div className="mb-4">
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-                <input type="email" name="email" id="email" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-md focus:outline-none" />
+                <input {...register("email")} type="email" name="email" id="email" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-md focus:outline-none" />
+                {errors.email && <p className="text-red-500 text-xs italic">{errors.email.message}</p>}
               </div>
             </div>
             <div className="rounded-md shadow-md p-3">
               <div className="mb-4">
                 <label htmlFor="website" className="block text-sm font-medium text-gray-700">Site Web</label>
-                <input type="url" name="website" id="website" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-md focus:outline-none" />
+                <input {...register("website")}type="url" name="website" id="website" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-md focus:outline-none" />
+                {errors.website && <p className="text-red-500 text-xs italic">{errors.website.message}</p>}
               </div>
             </div>
             <div>
--- src/validations/index.ts
+++ src/validations/index.ts
@@ -4,5 +4,15 @@ export const userSchema = yup.object({
   name: yup.string().required('Name is required').min(3, 'Name must be at least 3 characters').max(5, 'Name must not exceed 5 characters'),
   age: yup.number().required('Age is required').positive('Age must be positive').integer('Age must be an integer'),
   email: yup.string().email('Invalid email').required('Email is required'),
-  website: yup.string().url('Invalid URL').required('Website is required')
+  website: yup.string().url('Invalid URL').required('Website is required'),
+  createdOn: yup.date().default(function () {
+    return new Date();
+  }),
+  sex: yup.mixed().oneOf(["männlich", "weiblich", "divers"] as const).defined(),
+  tel: yup.string().matches(/^(0 |\+33)[1-9]\d{8}$/, 'Invalid phone number').required('Phone number is required'),
+  address: yup.object({
+    street: yup.string().required('Street is required'),
+    city: yup.string().required('City is required'),
+    zip: yup.string().matches(/^[0-9]{5}$/, 'Invalid ZIP').required('ZIP is required')
+  })
 });
